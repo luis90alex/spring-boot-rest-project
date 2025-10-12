4f435a9fb327081c69bd31f5880a8721f4016c31
luis90alex <luis90alex@gmail.com>
2025-10-12 17:13:25 +0200

    Added AuthenticatorManager,AuthenticationProvider , a jwt generator, env.example and validated manually jwt with class Jwts

--- src/main/java/com/restlearningjourney/store/controllers/AuthController.java
+++ src/main/java/com/restlearningjourney/store/controllers/AuthController.java
@@ -1,16 +1,18 @@
 package com.restlearningjourney.store.controllers;
 
+import com.restlearningjourney.store.dtos.JwtResponse;
 import com.restlearningjourney.store.dtos.LoginRequest;
 import com.restlearningjourney.store.repositories.UserRepository;
+import com.restlearningjourney.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,17 +21,30 @@ public class AuthController {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(
+    public ResponseEntity<JwtResponse> login(
             @RequestBody @Valid LoginRequest request){
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        return ResponseEntity.ok().build();
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var token = jwtService.generateJwtToken(request.getEmail());
+        System.out.println(token);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("/validate")
+    public boolean validate(@RequestHeader("Authorization") String authHeader){
+        var token = authHeader.replace("Bearer ", "");
+        return jwtService.validateToken(token);
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
---
+++ src/main/java/com/restlearningjourney/store/services/JwtService.java
@@ -0,0 +1,38 @@
+package com.restlearningjourney.store.services;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${spring.jwt.secret}")
+    private String secret;
+    public String generateJwtToken(String email) {
+
+        final long tokenExpiration = 86400; // 1 day
+        return Jwts.builder()
+                .subject(email)  //sub property of jwt
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + tokenExpiration*1000))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+       try {
+           Claims claims = Jwts.parser()
+                   .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                   .build()
+                   .parseSignedClaims(token)
+                   .getPayload();
+           return claims.getExpiration().after(new Date());
+       }catch (JwtException e){
+           return false;
+       }
+    }
+}
