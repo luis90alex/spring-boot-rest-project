41961f2e2824091da8e666673c3df32e23180465
luis90alex <luis90alex@gmail.com>
2025-12-27 17:21:23 +0100

    Modified stripe config to avoid loading it in integration tests

--- src/main/java/com/restlearningjourney/store/payments/StripeConfig.java
+++ src/main/java/com/restlearningjourney/store/payments/StripeConfig.java
@@ -4,8 +4,10 @@ import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test & !test-container")   //don't use it in unit integration profiles
 public class StripeConfig {
 
     @Value("${stripe.secretKey}")
